991fabce06fb1e054adc0b21d0527ec43ea77aa0
Manuel Duarte  <carlosd99m@gmail.com>
2025-03-21 03:20:23 -0600

    add ShimmerEffect (#3)

--- app/src/main/java/dev/donmanuel/kotlinandroidtemplate/presentation/screens/XboxScreens.kt
+++ app/src/main/java/dev/donmanuel/kotlinandroidtemplate/presentation/screens/XboxScreens.kt
@@ -1,8 +1,12 @@
 package dev.donmanuel.kotlinandroidtemplate.presentation.screens
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -11,10 +15,12 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import dev.donmanuel.kotlinandroidtemplate.presentation.composables.CustomTopAppBar
 import dev.donmanuel.kotlinandroidtemplate.presentation.composables.GameCard
+import dev.donmanuel.kotlinandroidtemplate.presentation.composables.ShimmerEffect
 import dev.donmanuel.kotlinandroidtemplate.presentation.viewmodels.GameViewModel
 
 
@@ -24,6 +30,7 @@ fun XboxScreens(
 ) {
 
     val games = viewModel.xboxGames.collectAsState().value
+    val isLoading = viewModel.isLoading.collectAsState().value
 
     var showSearch by remember { mutableStateOf(false) }
     var searchText by remember { mutableStateOf("") }
@@ -46,8 +53,23 @@ fun XboxScreens(
                 .padding(paddingValues)
                 .padding(16.dp)
         ) {
-            items(games) { game ->
-                GameCard(game = game)
+            if (isLoading) {
+                item(5) {
+                    ShimmerEffect(
+                        modifier = Modifier
+                            .padding(bottom = 16.dp)
+                            .fillMaxWidth()
+                            .height(60.dp)
+                            .background(
+                                Color.LightGray,
+                                shape = RoundedCornerShape(12.dp)
+                            )
+                    )
+                }
+            } else {
+                items(games) { game ->
+                    GameCard(game = game)
+                }
             }
         }
     }
