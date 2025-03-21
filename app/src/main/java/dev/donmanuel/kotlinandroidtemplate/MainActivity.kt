287acbfa87808bf14fa59308be2724f4d132f3c1
manuelduarte077 <carlosd99m@gmail.com>
2025-03-21 02:39:31 -0600

    refactor code

--- app/src/main/java/dev/donmanuel/kotlinandroidtemplate/MainActivity.kt
+++ app/src/main/java/dev/donmanuel/kotlinandroidtemplate/MainActivity.kt
@@ -1,18 +1,12 @@
 package dev.donmanuel.kotlinandroidtemplate
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
@@ -41,6 +35,7 @@ class MainActivity : ComponentActivity() {
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun BottomNavScreen() {
     var selectedItem by remember { mutableIntStateOf(0) }
@@ -55,15 +50,15 @@ fun BottomNavScreen() {
                 }
             )
         }
-    ) { paddingValues ->
-        ContentScreen(selectedItem, paddingValues)
+    ) {
+        ContentScreen(selectedItem)
     }
 
 }
 
 
 @Composable
-fun ContentScreen(selectedItemId: Int, paddingValues: PaddingValues) {
+fun ContentScreen(selectedItemId: Int) {
     when (selectedItemId) {
         0 -> XboxScreens()
         1 -> SwitchScreens()
--- app/src/main/java/dev/donmanuel/kotlinandroidtemplate/domain/model/ReleaseDates.kt
+++
@@ -1,8 +0,0 @@
-package dev.donmanuel.kotlinandroidtemplate.domain.model
-
-data class ReleaseDates(
-    val Australia: String,
-    val Europe: String,
-    val Japan: String,
-    val NorthAmerica: String
-)
